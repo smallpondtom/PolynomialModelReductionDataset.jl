f1ce4609d5d9b53f1496cd2cd1be82e72287cf3d
smallpondtom <tkoike45@gmail.com>
2024-10-06 21:43:27 -0400

    change requirement for SparseArrays

--- docs/make.jl
+++ docs/make.jl
@@ -36,7 +36,7 @@ makedocs(
     repo = Remotes.GitHub("smallpondtom", "PolynomialModelReductionDataset.jl"),
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        edit_link = "https://github.com/smallpondtom/PoMoReDa",
+        edit_link = "https://github.com/smallpondtom/PolynomialModelReductionDataset.jl",
         assets=String[
             "assets/citations.css",
             "assets/favicon.ico",
